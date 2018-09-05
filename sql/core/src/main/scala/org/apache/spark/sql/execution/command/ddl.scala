c7627db2d79376995eb20f0f137409220b07550d
Marco Gaido <marcogaido91@gmail.com>
2018-09-05 18:11:38 +0200

    adding UT from SPARK-14922 by DazhuangSu

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/ddl.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/ddl.scala
@@ -32,7 +32,7 @@ import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.analysis.Resolver
 import org.apache.spark.sql.catalyst.catalog._
 import org.apache.spark.sql.catalyst.catalog.CatalogTypes.{PartitionFiltersSpec, TablePartitionSpec}
-import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference, Cast, EqualNullSafe, EqualTo, GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, Literal, Not}
+import org.apache.spark.sql.catalyst.expressions.{And, Attribute, AttributeReference, Cast, EqualNullSafe, EqualTo, GreaterThan, GreaterThanOrEqual, LessThan, LessThanOrEqual, Literal, Not}
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, LogicalRelation, PartitioningUtils}
 import org.apache.spark.sql.execution.datasources.orc.OrcFileFormat
@@ -546,7 +546,8 @@ case class AlterTableDropPartitionCommand(
           table.identifier,
           catalog,
           sparkSession.sessionState.conf.resolver,
-          timeZone)
+          timeZone,
+          ifExists)
       } else {
         val partitionSpec = filtersSpec.map {
           case (key, _, value) => key -> value
@@ -579,7 +580,8 @@ case class AlterTableDropPartitionCommand(
       tableIdentifier: TableIdentifier,
       catalog: SessionCatalog,
       resolver: Resolver,
-      timeZone: Option[String]): Seq[TablePartitionSpec] = {
+      timeZone: Option[String],
+      ifExists: Boolean): Seq[TablePartitionSpec] = {
     val filters = partitionFilterSpec.map { case (partitionColumn, operator, value) =>
       val normalizedPartition = PartitioningUtils.normalizePartitionColumn(
         partitionColumn,
@@ -606,6 +608,9 @@ case class AlterTableDropPartitionCommand(
       }
     }
     val partitions = catalog.listPartitionsByFilter(tableIdentifier, filters)
+    if (partitions.isEmpty && !ifExists) {
+      throw new AnalysisException(s"There is no partition for ${filters.reduceLeft(And).sql}")
+    }
     partitions.map(_.spec)
   }
 }
