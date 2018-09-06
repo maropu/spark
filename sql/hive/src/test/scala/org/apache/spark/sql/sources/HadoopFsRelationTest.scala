98da86d63640c1843797478c31647e1d96bd8be2
Takeshi Yamamuro <yamamuro@apache.org>
2018-09-06 23:48:04 +0900

    Implement InterpretedSafeProjection

--- sql/hive/src/test/scala/org/apache/spark/sql/sources/HadoopFsRelationTest.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/sources/HadoopFsRelationTest.scala
@@ -111,7 +111,7 @@ abstract class HadoopFsRelationTest extends QueryTest with SQLTestUtils with Tes
     new StructType()
       .add("f1", FloatType, nullable = true)
       .add("f2", ArrayType(BooleanType, containsNull = true), nullable = true),
-    new UDT.MyDenseVectorUDT()
+    new TestUDT.MyDenseVectorUDT()
   ).filter(supportsDataType)
 
   for (dataType <- supportedDataTypes) {
